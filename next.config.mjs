39c1ca10ed7ba15c0e3f3627649ba51b0952643a
Juan C Muñoz <juancarlosmpdev@gmail.com>
2025-03-30 16:48:06 -0500

    Actualización de la configuración de Next.js para utilizar una URL estática de CloudFront en lugar de una variable de entorno. Esto mejora la consistencia en la carga de recursos remotos.

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_CLOUDFRONT_URL || '',
+        hostname: "d20iz9a0oa67fs.cloudfront.net",
       },
     ],
   },
